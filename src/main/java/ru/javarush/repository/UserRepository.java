817e1a8a43c9902007a0b85bccf72894f87cf2c9
hasimovagm <gunay.hasimova@kapitalbank.az>
2023-12-07 21:19:31 +0400

    add new method for getting user by age

--- src/main/java/ru/javarush/repository/UserRepository.java
+++ src/main/java/ru/javarush/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package ru.javarush.repository;
 
+import java.util.Optional;
 import ru.javarush.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> getByAge(Integer age);
 
 }
